dfc026231c626c337b3ba34102b9d97ea90da988
Dwalli <92029006+Dwalli@users.noreply.github.com>
2025-08-28 03:10:08 +0100

    color

--- Shape_and_Physics/Shape_and_Physics/main.cpp
+++ Shape_and_Physics/Shape_and_Physics/main.cpp
@@ -1,28 +1,57 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 
 int main(void)
 {
+    /* Initialize the library */
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  /* For this to work on mac uncomment this code */
+
+
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1600, 1024, "Shape and Physics", NULL, NULL);
+
     if (!window)
     {
+        std::cout << "Failed to creat GLFW Window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
+        std::cout << "Failed to Load GLAD" << std::endl;
+        return -1;
+
+    }
+
+
+     /* The size of the rendering window */
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+
+        glClearColor(0.7f, 0.0f, 0.8f, 0.4f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
